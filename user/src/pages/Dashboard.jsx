ccd5ca60656ed36186261f820df992e4f1779f76
OmarBuena <omarbuena18@gmail.com>
2023-10-16 21:16:03 +0800

    first commit

---
+++ user/src/pages/Dashboard.jsx
@@ -0,0 +1,37 @@
+import Welcome from "../components/Welcome";
+import AddAccident from "../components/accident/AddAccident";
+import AddCrime from "../components/crimes/AddCrime";
+import { useNavigate } from "react-router-dom";
+
+const Dashboard = () => {
+  const navigate = useNavigate("/");
+  const handleLogout = async () => {
+    alert("Are you sure you want to logout");
+    navigate("/");
+    const token = localStorage.getItem("token");
+    await axios.delete(`http://localhost:8080/api/users/logout`, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+  };
+
+  return (
+    <div className="flex flex-col h-screen justify-center p-2">
+      <button
+        onClick={handleLogout}
+        className="border rounded w-[200px] m-2 px-4 py-2 bg-red-500 text-white hover:bg-red-600">
+        Logout
+      </button>
+      <Welcome />
+
+        <div className="flex flex-col gap-3 p-5">
+          <AddAccident />
+          <AddCrime />
+        </div>
+    </div>
+  );
+  
+};
+
+export default Dashboard;
